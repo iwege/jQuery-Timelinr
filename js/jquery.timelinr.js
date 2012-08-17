0042205d369dceb99c3778fad78a0e7dbb90f6be
William Ge <weix.ge@intel.com>
2012-08-17 11:40:11 +0800

    remove autoplay; now we use class to find element now;

--- js/jquery.timelinr.js
+++ js/jquery.timelinr.js
@@ -13,17 +13,17 @@ jQuery.fn.timelinr = function(options){
 	// default plugin settings
 	settings = jQuery.extend({
 		orientation: 				'horizontal',		// value: horizontal | vertical, default to horizontal
-		containerDiv: 				'#timeline',		// value: any HTML tag or #id, default to #timeline
-		datesDiv: 					'#dates',			// value: any HTML tag or #id, default to #dates
+		containerDiv: 				'.timeline',		// value: any HTML tag or #id, default to #timeline
+		datesDiv: 					'.dates',			// value: any HTML tag or #id, default to #dates
 		datesSelectedClass: 		'selected',			// value: any class, default to selected
 		datesSpeed: 				'normal',			// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to normal
-		issuesDiv: 					'#issues',			// value: any HTML tag or #id, default to #issues
+		issuesDiv: 					'.issues',			// value: any HTML tag or #id, default to #issues
 		issuesSelectedClass: 		'selected',			// value: any class, default to selected
 		issuesSpeed: 				'fast',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to fast
 		issuesTransparency: 		0.2,				// value: integer between 0 and 1 (recommended), default to 0.2
 		issuesTransparencySpeed: 	500,				// value: integer between 100 and 1000 (recommended), default to 500 (normal)
-		prevButton: 				'#prev',			// value: any HTML tag or #id, default to #prev
-		nextButton: 				'#next',			// value: any HTML tag or #id, default to #next
+		prevButton: 				'.prev',			// value: any HTML tag or #id, default to #prev
+		nextButton: 				'.next',			// value: any HTML tag or #id, default to #next
 		arrowKeys: 					'false',			// value: true | false, default to false
 		startAt: 					1,					// value: integer, default to 1 (first)
 		autoPlay: 					'false',			// value: true | false, default to false
@@ -162,54 +162,11 @@ jQuery.fn.timelinr = function(options){
 			}
 		});
 		
-		// keyboard navigation, added since 0.9.1
-		if(settings.arrowKeys=='true') {
-			if(settings.orientation=='horizontal') {
-				$(document).keydown(function(event){
-					if (event.keyCode == 39) { 
-				       $(settings.nextButton).click();
-				    }
-					if (event.keyCode == 37) { 
-				       $(settings.prevButton).click();
-				    }
-				});
-			} else if(settings.orientation=='vertical') {
-				$(document).keydown(function(event){
-					if (event.keyCode == 40) { 
-				       $(settings.nextButton).click();
-				    }
-					if (event.keyCode == 38) { 
-				       $(settings.prevButton).click();
-				    }
-				});
-			}
-		}
+
 		
 		// default position startAt, added since 0.9.3
 		$(settings.datesDiv+' li').eq(settings.startAt-1).find('a').trigger('click');
 		
-		// autoPlay, added since 0.9.4
-		if(settings.autoPlay == 'true') { 
-			setInterval("autoPlay()", settings.autoPlayPause);
-		}
 	});
 
 };
-
-// autoPlay, added since 0.9.4
-function autoPlay(){
-	var currentDate = $(settings.datesDiv).find('a.'+settings.datesSelectedClass);
-	if(settings.autoPlayDirection == 'forward') {
-		if(currentDate.parent().is('li:last-child')) {
-			$(settings.datesDiv+' li:first-child').find('a').trigger('click');
-		} else {
-			currentDate.parent().next().find('a').trigger('click');
-		}
-	} else if(settings.autoPlayDirection == 'backward') {
-		if(currentDate.parent().is('li:first-child')) {
-			$(settings.datesDiv+' li:last-child').find('a').trigger('click');
-		} else {
-			currentDate.parent().prev().find('a').trigger('click');
-		}
-	}
-}
